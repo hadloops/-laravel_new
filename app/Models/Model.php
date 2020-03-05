d53a5c458ab6b4c2d0086caa0418d8510c64dfe2
王星 <wxhuozhe@gmail.com>
2020-03-05 17:54:14 +0800

    up

---
+++ app/Models/Model.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+/**
+ * Class Model
+ * @package App\Models
+ */
+class Model extends EloquentModel
+{
+
+}
---
+++ app/Services/Service.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Http\Controllers\Controller;
+
+class Service extends Controller
+{
+    public function __construct()
+    {
+        // 初始化
+    }
+
+}
