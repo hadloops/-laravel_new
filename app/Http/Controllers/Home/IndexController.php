4cf47c552611e33f5ca4b26a4426a6b1ac2e363b
王星 <wxhuozhe@gmail.com>
2020-03-06 09:54:22 +0800

    up

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Jobs\User\SendMessage;
-use App\Loop\Log;
+use App\Loop\Log as LoopLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class IndexController extends Controller
 {
     //
     public function index()
     {
-        Log::error(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
+        Log::info(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
     }
 
 
@@ -20,7 +21,7 @@ class IndexController extends Controller
     {
         for ($i = 0; $i < 10; $i++) {
             $time = mt_rand(1, 10);
-            //Log::error(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
+            Log::error(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
 
             $this->dispatch((new SendMessage($time))->onQueue('user_login')->delay(now()->addSeconds($time)));
         }
