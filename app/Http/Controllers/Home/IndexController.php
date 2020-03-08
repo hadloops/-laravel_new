2473689a758c11b8d2a50c8554a6a093e9871e44
王星 <wxhuozhe@gmail.com>
2020-03-08 09:40:54 +0800

    update

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -19,12 +19,12 @@ class IndexController extends Controller
 
     public function run()
     {
-        for ($i = 0; $i < 10; $i++) {
-            $time = mt_rand(1, 10);
-            Log::error(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
 
-            $this->dispatch((new SendMessage($time))->onQueue('user_login')->delay(now()->addSeconds($time)));
-        }
+        $time = mt_rand(1, 10);
+        Log::error(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
+
+        $this->dispatch((new SendMessage($time))->onQueue('user_login')->delay(now()->addSeconds($time)));
+
 
     }
 }
