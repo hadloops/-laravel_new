c6ab425d8d189f4a8e517556430b671f6529398e
王星 <wxhuozhe@gmail.com>
2020-06-06 22:50:53 +0800

    up

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -8,7 +8,6 @@ use App\Loop\Log as LoopLog;
 use App\Services\AesService;
 use App\Services\ReaService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class IndexController extends Controller
 {
@@ -27,7 +26,7 @@ class IndexController extends Controller
     {
 
         $time = mt_rand(1, 10);
-        Log::info(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
+        info(sprintf("[%s] [%s]", __CLASS__, date("Y-m-d H:i:s")));
         return $this->dispatch((new SendMessage($time))->onQueue('user_login')->delay(now()->addSeconds($time)));
 
 
