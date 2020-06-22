4a1b91bfc7fd0ecfd57a68b0120e5e446a35f439
王星 <wxhuozhe@gmail.com>
2020-06-22 09:24:50 +0800

    up

---
+++ app/Library/Tool.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangxing
+ * Date: 2020/6/19
+ * Time: 5:31 下午
+ */
+
+namespace App\Library;
+
+
+class Tool implements ToolInterface
+{
+    public function execute(array $data = [])
+    {
+
+    }
+
+}
---
+++ app/Library/ToolInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangxing
+ * Date: 2020/6/19
+ * Time: 5:31 下午
+ */
+
+namespace App\Library;
+
+
+interface ToolInterface
+{
+    public function execute(array $data = []);
+
+}
