ba12832256c81b3f480431b7c8e9295c768bbb12
王星 <wxhuozhe@gmail.com>
2020-07-25 00:25:50 +0800

    upxs

--- app/Http/Controllers/Home/UserController.php
+++ app/Http/Controllers/Home/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
     public function test()
     {
        dump( $this->get_real_ip());
+       dump( $this->getRealIp());
 
 
        dump($this->GetIp());die;
@@ -133,6 +134,41 @@ class UserController extends Controller
         $realip = !empty($onlineip[0]) ? $onlineip[0] : '0.0.0.0';
         return $realip;
     }
+
+    public  function getRealIp()
+    {
+        if (isset($_SERVER)) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+                foreach ($arr as $ip) {
+                    $ip = trim($ip);
+
+                    if ($ip != 'unknown') {
+                        $realip = $ip;
+                        break;
+                    }
+                }
+            } else if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $realip = $_SERVER['HTTP_CLIENT_IP'];
+            } else if (isset($_SERVER['REMOTE_ADDR'])) {
+                $realip = $_SERVER['REMOTE_ADDR'];
+            } else {
+                $realip = '0.0.0.0';
+            }
+        } else if (getenv('HTTP_X_FORWARDED_FOR')) {
+            $realip = getenv('HTTP_X_FORWARDED_FOR');
+        } else if (getenv('HTTP_CLIENT_IP')) {
+            $realip = getenv('HTTP_CLIENT_IP');
+        } else {
+            $realip = getenv('REMOTE_ADDR');
+        }
+
+        preg_match('/[\\d\\.]{7,15}/', $realip, $onlineip);
+        $realip = (!empty($onlineip[0]) ? $onlineip[0] : '0.0.0.0');
+        return $realip;
+    }
+
 
 
 
