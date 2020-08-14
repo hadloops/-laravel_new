5497d6946e6a4040251dd15ea8c47f7c32a95ba5
王星 <wxhuozhe@gmail.com>
2020-08-14 11:27:59 +0800

    upxs

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,5 +5,14 @@ namespace App\Models;
 
 class UserModel extends Model
 {
-    //
+
+
+    # 库连接名，默认库，需要连接其他库，请修改此处并配置.env和database.php文件中的配置
+    protected $connection = 'mysql';
+
+    # 表名
+    protected $table = 'user_list';
+
+    # 不自动维护时间字段
+    public $timestamps = false;
 }
