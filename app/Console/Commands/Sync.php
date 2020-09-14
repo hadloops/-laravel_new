2a3cf183889f7da9b08f3787e96059303c2697af
王星 <wxhuozhe@gmail.com>
2020-09-14 12:24:49 +0800

    up

---
+++ app/Console/Commands/Sync.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Sync extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //
+        $this->sync(1,2);
+    }
+
+    public function sync($goodsId,$skuId){
+
+        var_dump(get_defined_vars());
+    }
+}
