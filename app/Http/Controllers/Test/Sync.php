4ac1b3c325e1c912e535ab9e00bd0982ac5c90e6
王星 <wxhuozhe@gmail.com>
2020-10-24 18:03:47 +0800

    up

--- app/Http/Controllers/Test/Sync.php
+++ app/Http/Controllers/Test/Sync.php
@@ -10,11 +10,87 @@ class Sync extends Controller
 {
    public function handle()
    {
-        $info = Stack::handle();
 
 
-        $flag = Stack::$openCache;
+       $str = '{
+    "top_data": {
+        "goods_list": [
+            {
+                "item_type": "",
+                "logo_url": "",
+                "url_xcx": "",
+                "url_h5": "",
+                "act_id": "",
+                "act_type": "",
+                "sku_id": "",
+                "goods_id": "",
+                "is_related": ""
+            }
+        ],
+        "goods_data": {
+            "item_type": "",
+            "logo_url": "",
+            "url_xcx": "",
+            "url_h5": "",
+            "act_id": "",
+            "act_type": "",
+            "sku_id": "",
+            "goods_id": "",
+            "is_related": ""
+        }
+    },
+    "bottom_data": {
+        "small_list": {
+            "is_flip": "",
+            "list": [
+                [
+                    {
+                        "item_type": "",
+                        "logo_url": "",
+                        "url_xcx": "",
+                        "url_h5": "",
+                        "act_id": "",
+                        "act_type": "",
+                        "is_related": "",
+                        "short_name": "",
+                        "goods_id": "",
+                        "sku_id": ""
+                    }
+                ]
+            ]
+        },
+        "big_list": {
+            "is_flip": "",
+            "list": [
+                [
+                    {
+                        "item_type": "",
+                        "logo_url": "",
+                        "url_xcx": "",
+                        "url_h5": "",
+                        "act_id": "",
+                        "act_type": "",
+                        "is_related": "",
+                        "short_name": "",
+                        "goods_id": "",
+                        "sku_id": ""
+                    }
+                ]
+            ]
+        }
+    }
+}';
 
-        var_dump($flag);die;
+        $arr = json_decode($str,true);
+
+
+        dd($arr);
+
+//        $info = Stack::handle();
+//
+//
+//        $flag = Stack::$openCache;
+//
+//        var_dump($flag);die;
    }
 }
